b7c3ae958ed9b4f8f66b1f5f2341e97495a10731
skiseiju <skiseiju@gmail.com>
2026-03-04 16:20:06 +0800

    release: v2.2.1

--- dist/extension/content.js
+++ dist/extension/content.js
@@ -1,9 +1,9 @@
 (function() {
     'use strict';
-    console.log('[HegeBlock] Content Script Injected, Version: 2.1.1-beta10');
+    console.log('[HegeBlock] Content Script Injected, Version: 2.2.1');
 // --- config.js ---
 const CONFIG = {
-    VERSION: '2.1.1-beta10', // Official Release: Worker UI 2.0 & Cooldown Protection
+    VERSION: '2.2.1', // Bug Report System & Stability
     DEBUG_MODE: true,
     DB_KEY: 'hege_block_db_v1',
     KEYS: {
--- dist/threads_block_tool.user.js
+++ dist/threads_block_tool.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         留友封 (Threads 封鎖工具)
 // @namespace    http://tampermonkey.net/
-// @version      2.1.1-beta10
+// @version      2.2.1
 // @description  Modular Refactor Build
 // @author       海哥
 // @match        https://www.threads.net/*
@@ -14,10 +14,10 @@
 
 (function() {
     'use strict';
-    console.log('[HegeBlock] Content Script Injected, Version: 2.1.1-beta10');
+    console.log('[HegeBlock] Content Script Injected, Version: 2.2.1');
 // --- config.js ---
 const CONFIG = {
-    VERSION: '2.1.1-beta10', // Official Release: Worker UI 2.0 & Cooldown Protection
+    VERSION: '2.2.1', // Bug Report System & Stability
     DEBUG_MODE: true,
     DB_KEY: 'hege_block_db_v1',
     KEYS: {
